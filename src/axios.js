09738580c4d94d5788c6101d1f7cdf1790af05e9
joaofranciscosousa <89461082+joaofranciscosousa@users.noreply.github.com>
2022-01-30 15:05:24 -0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
+    //localhost para testes
+    //baseURL: "http://localhost:9000/" 
     baseURL: "https://tiktok-back-test.herokuapp.com/"
 });
 
